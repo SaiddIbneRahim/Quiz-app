29ed6bc52081fa30684818d9d89190e5c051db95
Saidd Ibne Rahim <saiddibnerahim@gmail.com>
2025-04-29 21:33:15 +0530

    Add files via upload

---
+++ Quiz App/script.js
@@ -0,0 +1,110 @@
+const questions = [
+  {
+    question: "what is the capital of India ?",
+    answers: [
+      { text: "Mumbai", correct: false },
+      { text: "Delhi", correct: true },
+      { text: "Guwahati", correct: false },
+      { text: "Chennai", correct: false },
+    ],
+  },
+  {
+    question: "Who is the pm of India?",
+    answers: [
+      { text: "Saidd", correct: false },
+      { text: "Modi", correct: true },
+      { text: "Elvish", correct: false },
+      { text: "Neha kakkar", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHtml = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+
+    if(answer.correct){
+        button.dataset.correct = answer.correct;
+    }
+
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct ==="true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+
+}
+
+function showScore(){
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}` ;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+
+}
+
+function handlenextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex<questions.length){
+    showQuestion();
+  }else{
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", ()=>{
+  if(currentQuestionIndex < questions.length){
+    handlenextButton();
+  } else{
+    startQuiz();
+  }
+
+})
+
+startQuiz();
